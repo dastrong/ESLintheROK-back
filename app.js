4eff312831a68a3d10b76ddfade4e0cb8db33e80
Daniel Strong <danielstrong.ds@gmail.com>
2020-12-07 19:20:44 +0900

    made some changes while migrating from mLab to Atlas

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const express = require("express"),
-      app     = express(),
-      port    = process.env.PORT || 3000,
-      bodyParser = require("body-parser"),
-      gradeRoutes  = require('./routes/gradesR'),
-      bookRoutes   = require('./routes/booksR'),
-      lessonRoutes = require('./routes/lessonsR');
+require('dotenv').config()
 
-app.use(function(req, res, next) { 
-  res.header("Access-Control-Allow-Origin", "*"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
-  next(); 
-}); 
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const bodyParser = require("body-parser");
+const gradeRoutes = require('./routes/gradesR');
+const bookRoutes = require('./routes/booksR');
+const lessonRoutes = require('./routes/lessonsR');
+
+const port = process.env.PORT || 3000;
+const isDev = process.env.NODE_ENV === 'dev'
+const DBurl = process.env.DBurl;
+
+mongoose.set('debug', isDev);
+mongoose.connect(DBurl, { useNewUrlParser: true });
+mongoose.Promise = Promise;
+
+app.use(cors()); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// ======================//
 
-app.get('/', (req, res)=>{
-  res.send('ESL in the ROK API Main Page');
-});
-
-
+app.get('/', (req, res) => res.send('ESL in the ROK API Main Page'));
 app.use('/api', gradeRoutes);
 app.use('/api/:gradeID/:bookID', bookRoutes);
 app.use('/api/:gradeID/:bookID', lessonRoutes);
-// ======================//     
-app.listen(process.env.PORT, ()=>{
+
+app.listen(port, () => {
   console.log(`UP and RUNNING on port: ${port}`);
 });
--- models/index.js
+++ models/index.js
@@ -1,13 +1,9 @@
-const mongoose = require('mongoose'),
-      Grade    = require('./grade'),
-      Book     = require('./book'),
-      Lesson   = require('./lesson'),
-      DBurl    = process.env.NODE_ENV
-                  ? `mongodb://${process.env.DBuser}:${process.env.DBpw}@ds041167.mlab.com:41167/eslintherok`
-                  : 'mongodb://localhost:27017/lessons-api';
-      
-mongoose.set('debug', !process.env.NODE_ENV ? true : false);
-mongoose.connect(DBurl, { useNewUrlParser: true });
-mongoose.Promise = Promise;
+const Grade = require('./grade');
+const Book = require('./book');
+const Lesson = require('./lesson');
 
-module.exports = {Grade, Book, Lesson};
+module.exports = {
+  Grade, 
+  Book, 
+  Lesson
+};
