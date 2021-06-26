d694ae8af60fb6fb596597dd926f3d22e4089ef2
Daniel Strong <danielstrong.ds@gmail.com>
2021-06-26 17:30:36 +0900

    moved around route imports and added catch all and error routes

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
-const gradeRoutes = require('./routes/grades');
-const bookRoutes = require('./routes/books');
-const lessonRoutes = require('./routes/lessons');
-
 const isDev = process.env.NODE_ENV === 'dev';
 const port = process.env.PORT || 4000;
 const DBurl = process.env.DBurl;
@@ -21,10 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => res.send('ESL in the ROK API Main Page'));
-app.use('/api', gradeRoutes);
-app.use('/api/:gradeID/:bookID', bookRoutes);
-app.use('/api/:gradeID/:bookID', lessonRoutes);
+//load all the routes here
+require('./routes')(app);
 
 try {
   app.listen(port, () => {
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const createError = require('http-errors');
+const gradeRoutes = require('./grades');
+const bookRoutes = require('./books');
+const lessonRoutes = require('./lessons');
+
+module.exports = app => {
+  app.use('/api/grades', gradeRoutes);
+  app.use('/api/books', bookRoutes);
+  app.use('/api/lessons', lessonRoutes);
+
+  //catches all endpoints that don't exist above
+  app.use((req, res, next) =>
+    next(createError(404, `Invalid URL: ${req.url}`))
+  );
+
+  //catches all errors passed through next()
+  app.use((err, req, res, next) => {
+    const status = err.status || 400;
+    const message = err.message || 'Unknown Error';
+    res.status(status).json({ message });
+  });
+};
