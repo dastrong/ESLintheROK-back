1b18bf3a39607b2325098db7f49907d760d28dd0
Daniel Strong <danielstrong.ds@gmail.com>
2021-08-28 22:17:02 +0900

    added frontend test url to cors

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ mongoose.set('debug', isDev);
 mongoose.connect(DBurl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cookieParser());
-app.use(cors({ credentials: true }));
+app.use(
+  cors({
+    origin: 'https://eslintherok-dastrong.vercel.app',
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
