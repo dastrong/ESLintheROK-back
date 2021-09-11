f42ba9cdf44c8c6a9a6e5bfefe8f701a7b5c572c
Daniel Strong <danielstrong.ds@gmail.com>
2021-09-11 17:00:43 +0900

    removed cookie parser and cleaned up the app file

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const app = express();
 
-const isDev = process.env.NODE_ENV === 'dev';
-const port = process.env.PORT || 4000;
-const DBurl = process.env.DBurl;
+const DEV_ENV = process.env.NODE_ENV === 'dev';
+const PORT = process.env.PORT || 4000;
+const MONGO_URI = process.env.MONGO_URI;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-mongoose.set('debug', isDev);
-mongoose.connect(DBurl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('debug', DEV_ENV);
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: 'https://eslintherok-dastrong.vercel.app',
-    credentials: true,
-  })
-);
+app.use(cors());
+app.use(cors(!DEV_ENV ? { origin: FRONTEND_URL } : {}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,9 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 require('./routes')(app);
 
 try {
-  app.listen(port, () => {
-    console.log(`UP and RUNNING on port: ${port}`);
-  });
+  app.listen(PORT, () => console.log(`UP and RUNNING on port: ${PORT}`));
 } catch (err) {
   console.error(err);
 }
